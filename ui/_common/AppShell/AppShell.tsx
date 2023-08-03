9d3c4cb09c7d3fe387978ca890a847041333374b
Michael Rawlings <mirawlings@ebay.com>
2023-08-03 16:06:30 -0400

    fix: more menu spacing/overflow fixes

--- ui/_common/AppShell/AppShell.tsx
+++ ui/_common/AppShell/AppShell.tsx
@@ -6,7 +6,7 @@ export function AppShell(props) {
       <div class="flex-1 overflow-y-auto">
         <Outlet/>
       </div>
-      <div class="flex bg-gray-800 justify-center">
+      <div class="flex bg-gray-800 justify-stretch">
         <NavItem href="/activity">
           Activity
         </NavItem>
@@ -28,7 +28,7 @@ export function AppShell(props) {
 }
 function NavItem(props) {
   return (
-    <A href={props.href} class="text-sm p-4 font-medium" activeClass="text-primary-500" inactiveClass="text-gray-500 hover:text-gray-400 cursor-pointer">
+    <A href={props.href} class="text-sm flex-1 py-4 font-medium text-center" activeClass="text-primary-500" inactiveClass="text-gray-500 hover:text-gray-400 cursor-pointer">
       {props.children}
     </A>
   )
