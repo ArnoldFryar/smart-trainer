84694ba724c8fd164a94e909b021d907a031ace0
Michael Rawlings <mirawlings@ebay.com>
2023-08-03 09:06:30 -0400

    feat: sets per day (last 7 days), simplify history list items

--- ui/Activity/Activity.tsx
+++ ui/Activity/Activity.tsx
@@ -4,25 +4,56 @@ import { getSets } from '../../services/db/settings.js';
 import { WORKOUT_MODE_CONFIGS } from '../../services/workout/modes.js';
 import { A, useNavigate } from '@solidjs/router';
 
+const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
 export default function Activity() {
   const navigate = useNavigate();
   const [sets] = createResource(() => {
     return getSets("MICHAEL");
   });
-  const groups = () => Object.entries((sets()??[]).reduce((groups, set) => {
+  const groups = () => (sets()??[]).reduce((groups, set) => {
     const date = new Date(set.time).toDateString();
     if (!groups[date]) {
       groups[date] = [];
     }
     groups[date].push(set);
     return groups;
-  }, {}));
+  }, {});
+  const pastWeek = () => {
+    const today = new Date();
+    const pastWeek = [];
+    const setsByDate = groups();
+    for (let i = 0; i < 7; i++) {
+      const date = new Date(today.getFullYear(), today.getMonth(), today.getDate() - i);
+      pastWeek.push({
+        label: i === 0 ? "Today" : DAYS[date.getDay()],
+        setCount: setsByDate[date.toDateString()]?.length ?? 0,
+      });
+    }
+    return pastWeek.reverse();
+  }
   const today = new Date().toDateString();
   const yesterday = new Date(Date.now() - 24 * 60 * 60 * 1000).toDateString();
   return (
     <div class="flex flex-col p-4 h-full">
       <div class="grow text-sm overflow-y-auto">
-        <For each={groups()}>{([date, sets]) => (
+        <div class="flex justify-between content-between">
+          <For each={pastWeek()}>
+            {({ label, setCount }) => (
+              <div>
+                <div class={`flex items-center justify-center rounded-full w-12 h-12 relative text-lg ${setCount ? "bg-primary-800 text-white" : "bg-gray-800 text-gray-400"}`}>
+                  <span class={`absolute px-3 font-medium -translate-x-1/2 left-1/2`}>
+                    {setCount}
+                  </span>
+                </div>
+                <div class="w-12 text-center text-xs mt-2">
+                  {label}
+                </div>
+              </div>
+            )}
+          </For>
+        </div>
+        <For each={Object.entries(groups())}>{([date, sets]) => (
           <>
             <div class="inline-flex items-center justify-center w-full relative">
                 <hr class="w-64 h-px my-8 border-0 bg-gray-700"/>
@@ -31,13 +62,9 @@ export default function Activity() {
                 </span>
             </div>
             <For each={sets}>{(set) => (
-              <A href={`/set/${set._id}`} class="block m-2 p-4 bg-gray-800 rounded-sm relative">
-                <time class="text-xs absolute right-4 text-gray-400">{(new Date(set.time)).toLocaleTimeString()}</time>
+              <A href={`/set/${set._id}`} class="block m-2 p-4 bg-gray-800 rounded relative">
+                <span class="text-sm absolute right-4">{set.stats.reps} <span class="text-gray-300">x</span> {toLbs(set.stats.maxMinForce)}<span class="text-gray-300">lbs</span></span>
                 <div class="font-medium">{set.exercise_id}</div>
-                <div>
-                  <span>{set.stats.reps} x {toLbs(set.stats.maxMinForce)}lbs</span>
-                  <span class="text-gray-400"> ({WORKOUT_MODE_CONFIGS[set.mode].name})</span>
-                </div>
               </A>
             )}</For>
           </>
